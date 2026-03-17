fde4b35f8dc1eb8d156b474fa1a3ce63f3caef17
Meqdad-Moradi <meqdad.moradi@outlook.com>
2026-03-17 22:41:12 +0100

    some clean up

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { MainTitle } from "../../apps/main-title/main-title";
 
 @Component({
   selector: 'app-home',
-  imports: [MainTitle],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-
-}
+export class Home {}
