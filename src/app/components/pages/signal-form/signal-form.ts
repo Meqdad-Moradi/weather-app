99aebe3ea49e9a3c9658d4e28202640fb86e6b25
Meqdad-Moradi <meqdad.moradi@outlook.com>
2026-07-12 22:49:57 +0200

    signal form

--- src/app/components/pages/signal-form/signal-form.ts
+++ src/app/components/pages/signal-form/signal-form.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MainTitle } from "../../apps/main-title/main-title";
 
 @Component({
   selector: 'app-signal-form',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MainTitle],
   templateUrl: './signal-form.html',
   styleUrl: './signal-form.css',
 })
-export class SignalForm {
-
-}
+export class SignalForm {}
