c6d84d774e4b0e01219d85bb51d4d9d52ab2dacd
Meqdad-Moradi <meqdad.moradi@outlook.com>
2026-09-13 21:46:06 +0200

    login form updated + signal form added

--- src/app/components/pages/login/login.ts
+++ src/app/components/pages/login/login.ts
@@ -1,10 +1,14 @@
 import { Component, inject, signal } from '@angular/core';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Auth } from '../../../services/auth';
+import { Logo } from '../../apps/logo/logo';
+import { email, form, FormField, FormRoot, required } from '@angular/forms/signals';
+import { firstValueFrom } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [Logo, FormField, FormRoot, MatIconModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -12,31 +16,59 @@ export class Login {
   private readonly auth = inject(Auth);
   private readonly route = inject(ActivatedRoute);
   private readonly router = inject(Router);
-  readonly isLoading = signal(false);
-  readonly errorMessage = signal('');
 
-  submitLogin(event: SubmitEvent): void {
-    event.preventDefault();
-    const form = event.target as HTMLFormElement;
-    const formData = new FormData(form);
-    const email = String(formData.get('email') ?? '')
-      .trim()
-      .toLowerCase();
-    const password = String(formData.get('password') ?? '');
+  private loginModel = signal({
+    email: '',
+    password: '',
+    rememberMe: true,
+  });
 
-    this.errorMessage.set('');
-    this.isLoading.set(true);
+  protected loginForm = form(
+    this.loginModel,
+    (schemaPath) => {
+      required(schemaPath.email, { message: 'Email is required' });
+      email(schemaPath.email, { message: 'Email is not valid' });
+      required(schemaPath.password, { message: 'Password is required' });
+    },
+    {
+      submission: {
+        action: async (formData) => {
+          try {
+            const result = await firstValueFrom(
+              this.auth.login(formData.email().value(), formData.password().value()),
+            );
 
-    this.auth.login(email, password).subscribe((isAuthenticated) => {
-      this.isLoading.set(false);
+            console.log('Login result:', result);
 
-      if (!isAuthenticated) {
-        this.errorMessage.set('We could not sign you in with those details.');
-        return;
-      }
+            if (result) {
+              const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') ?? '/';
+              void this.router.navigateByUrl(returnUrl);
+            }
+            return;
+          } catch (error) {
+            return { kind: 'submissionError', message: 'Form submission failed' };
+          }
+        },
+        onInvalid: (formData) => {
+          const errors = formData().errorSummary();
 
-      const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') ?? '/';
-      void this.router.navigateByUrl(returnUrl);
+          if (errors.length > 0) {
+            const firstError = errors[0];
+            firstError.fieldTree().focusBoundControl();
+          }
+        },
+        ignoreValidators: 'none',
+      },
+    },
+  );
+
+  /**
+   * resetField
+   * @param filedName string
+   */
+  protected resetField(filedName: string): void {
+    this.loginModel.update((model) => {
+      return { ...model, [filedName]: '' };
     });
   }
 }
